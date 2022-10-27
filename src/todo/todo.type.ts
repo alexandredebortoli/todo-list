cd012e0c48e8e8f9cd40c16c2d377dcf73b4787b
alexandredebortoli <souza.alexandre@edu.univali.br>
2022-10-27 14:12:26 -0300

    fix: formatted files with prettier

--- src/todo/todo.type.ts
+++ src/todo/todo.type.ts
@@ -1,7 +1,7 @@
 export type createType = {
-  title: string,
-  description: string,
-  time: string,
-  completed: boolean,
-  uid: string
-}
+  title: string;
+  description: string;
+  time: string;
+  completed: boolean;
+  uid: string;
+};
